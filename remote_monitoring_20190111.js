85cbfd3149caee8c2e70307f239cf1d4595ceba0
yuki <46600832+yukit7s@users.noreply.github.com>
2019-01-15 09:13:40 +0900

    Add files via upload

---
+++ remote_monitoring_20190111.js
@@ -0,0 +1,292 @@
+//文字コード UTF-8
+//改行コード LF
+//https://docs.microsoft.com/ja-jp/azure/iot-accelerators/iot-accelerators-connecting-pi-node
+
+var Protocol = require('azure-iot-device-mqtt').Mqtt;
+var Client = require('azure-iot-device').Client;
+var ConnectionString = require('azure-iot-device').ConnectionString;
+var Message = require('azure-iot-device').Message;
+var async = require('async');
+
+var connectionString = 'HostName=azureiotdemo20181213be3ee.azure-devices.net;DeviceId=physical-chiller-ishida;SharedAccessKey=S2aYzt1SNnvV6JYEiKrLgE6ZkZRsUvHUWPo/Dw1wSPc=';
+var deviceId = ConnectionString.parse(connectionString).DeviceId;
+
+var temperature = 50;
+var temperatureUnit = 'F';
+var humidity = 50;
+var humidityUnit = '%';
+var pressure = 55;
+var pressureUnit = 'psig';
+
+var temperatureSchema = 'chiller-temperature;v1';
+var humiditySchema = 'chiller-humidity;v1';
+var pressureSchema = 'chiller-pressure;v1';
+var interval = "00:00:05";
+var deviceType = "Chiller";
+var deviceFirmware = "1.0.0";
+var deviceFirmwareUpdateStatus = "";
+var deviceLocation = "Building 44";
+var deviceLatitude = 47.638928;
+var deviceLongitude = -122.13476;
+var deviceOnline = true;
+
+var reportedProperties = {
+  "Protocol": "MQTT",
+  "SupportedMethods": "Reboot,FirmwareUpdate,EmergencyValveRelease,IncreasePressure,LED-ON,LED-OFF",
+  "Telemetry": {
+    "TemperatureSchema": {
+      "Interval": interval,
+      "MessageTemplate": "{\"temperature\":${temperature},\"temperature_unit\":\"${temperature_unit}\"}",
+      "MessageSchema": {
+        "Name": temperatureSchema,
+        "Format": "JSON",
+        "Fields": {
+          "temperature": "Double",
+          "temperature_unit": "Text"
+        }
+      }
+    },
+    "HumiditySchema": {
+      "Interval": interval,
+      "MessageTemplate": "{\"humidity\":${humidity},\"humidity_unit\":\"${humidity_unit}\"}",
+      "MessageSchema": {
+        "Name": humiditySchema,
+        "Format": "JSON",
+        "Fields": {
+          "humidity": "Double",
+          "humidity_unit": "Text"
+        }
+      }
+    },
+    "PressureSchema": {
+      "Interval": interval,
+      "MessageTemplate": "{\"pressure\":${pressure},\"pressure_unit\":\"${pressure_unit}\"}",
+      "MessageSchema": {
+        "Name": pressureSchema,
+        "Format": "JSON",
+        "Fields": {
+          "pressure": "Double",
+          "pressure_unit": "Text"
+        }
+      }
+    }
+  },
+  "Type": deviceType,
+  "Firmware": deviceFirmware,
+  "FirmwareUpdateStatus": deviceFirmwareUpdateStatus,
+  "Location": deviceLocation,
+  "Latitude": deviceLatitude,
+  "Longitude": deviceLongitude,
+  "Online": deviceOnline
+}
+
+function printErrorFor(op) {
+    return function printError(err) {
+        if (err) console.log(op + ' error: ' + err.toString());
+    };
+}
+
+function generateRandomIncrement() {
+    return ((Math.random() * 2) - 1);
+}
+
+function onDirectMethod(request, response) {
+  // Implement logic asynchronously here.
+  console.log('Simulated ' + request.methodName);
+
+  // Complete the response
+  response.send(200, request.methodName + ' was called on the device', function (err) {
+    if (err) console.error('Error sending method response :\n' + err.toString());
+    else console.log('200 Response to method \'' + request.methodName + '\' sent successfully.');
+  });
+}
+
+function onFirmwareUpdate(request, response) {
+  // Get the requested firmware version from the JSON request body
+  var firmwareVersion = request.payload.Firmware;
+  var firmwareUri = request.payload.FirmwareUri;
+
+  // Ensure we got a firmware values
+  if (!firmwareVersion || !firmwareUri) {
+    response.send(400, 'Missing firmware value', function(err) {
+      if (err) console.error('Error sending method response :\n' + err.toString());
+      else console.log('400 Response to method \'' + request.methodName + '\' sent successfully.');
+    });
+  } else {
+    // Respond the cloud app for the device method
+    response.send(200, 'Firmware update started.', function(err) {
+      if (err) console.error('Error sending method response :\n' + err.toString());
+      else {
+        console.log('200 Response to method \'' + request.methodName + '\' sent successfully.');
+
+        // Run the simulated firmware update flow
+        runFirmwareUpdateFlow(firmwareVersion, firmwareUri);
+      }
+    });
+  }
+}
+
+// Simulated firmwareUpdate flow
+function runFirmwareUpdateFlow(firmwareVersion, firmwareUri) {
+  console.log('Simulating firmware update flow...');
+  console.log('> Firmware version passed: ' + firmwareVersion);
+  console.log('> Firmware URI passed: ' + firmwareUri);
+  async.waterfall([
+    function (callback) {
+      console.log("Image downloading from " + firmwareUri);
+      var patch = {
+        FirmwareUpdateStatus: 'Downloading image..'
+      };
+      reportUpdateThroughTwin(patch, callback);
+      sleep(10000, callback);
+    },
+    function (callback) {
+      console.log("Downloaded, applying firmware " + firmwareVersion);
+      deviceOnline = false;
+      var patch = {
+        FirmwareUpdateStatus: 'Applying firmware..',
+        Online: false
+      };
+      reportUpdateThroughTwin(patch, callback);
+      sleep(8000, callback);
+    },
+    function (callback) {
+      console.log("Rebooting");
+      var patch = {
+        FirmwareUpdateStatus: 'Rebooting..'
+      };
+      reportUpdateThroughTwin(patch, callback);
+      sleep(10000, callback);
+    },
+    function (callback) {
+      console.log("Firmware updated to " + firmwareVersion);
+      deviceOnline = true;
+      var patch = {
+        FirmwareUpdateStatus: 'Firmware updated',
+        Online: true,
+        Firmware: firmwareVersion
+      };
+      reportUpdateThroughTwin(patch, callback);
+      callback(null);
+    }
+  ], function(err) {
+    if (err) {
+      console.error('Error in simulated firmware update flow: ' + err.message);
+    } else {
+      console.log("Completed simulated firmware update flow");
+    }
+  });
+
+  // Helper function to update the twin reported properties.
+  function reportUpdateThroughTwin(patch, callback) {
+    console.log("Sending...");
+    console.log(JSON.stringify(patch, null, 2));
+    client.getTwin(function(err, twin) {
+      if (!err) {
+        twin.properties.reported.update(patch, function(err) {
+          if (err) callback(err);
+        });      
+      } else {
+        if (err) callback(err);
+      }
+    });
+  }
+
+  function sleep(milliseconds, callback) {
+    console.log("Simulate a delay (milleseconds): " + milliseconds);
+    setTimeout(function () {
+      callback(null);
+    }, milliseconds);
+  }
+}
+
+function sendTelemetry(data, schema) {
+  if (deviceOnline) {
+    var d = new Date();
+    var payload = JSON.stringify(data);
+    var message = new Message(payload);
+    message.properties.add('$$CreationTimeUtc', d.toISOString());
+    message.properties.add('$$MessageSchema', schema);
+    message.properties.add('$$ContentType', 'JSON');
+
+    console.log('Sending device message data:\n' + payload);
+    client.sendEvent(message, printErrorFor('send event'));
+  } else {
+    console.log('Offline, not sending telemetry');
+  }
+}
+
+var client = Client.fromConnectionString(connectionString, Protocol);
+
+client.open(function (err) {
+if (err) {
+  printErrorFor('open')(err);
+} else {
+  // Create device Twin
+  client.getTwin(function (err, twin) {
+    if (err) {
+      console.error('Could not get device twin');
+    } else {
+      console.log('Device twin created');
+
+      twin.on('properties.desired', function (delta) {
+        // Handle desired properties set by solution
+        console.log('Received new desired properties:');
+        console.log(JSON.stringify(delta));
+      });
+
+      // Send reported properties
+      twin.properties.reported.update(reportedProperties, function (err) {
+        if (err) throw err;
+        console.log('Twin state reported');
+      });
+
+      // Register handlers for all the method names we are interested in.
+      // Consider separate handlers for each method.
+      client.onDeviceMethod('Reboot', onDirectMethod);
+      client.onDeviceMethod('FirmwareUpdate', onFirmwareUpdate);
+      client.onDeviceMethod('EmergencyValveRelease', onDirectMethod);
+      client.onDeviceMethod('IncreasePressure', onDirectMethod);
+      client.onDeviceMethod('LED-ON', onDirectMethod);
+      client.onDeviceMethod('LED-OFF', onDirectMethod);
+
+    }
+  });
+
+  // Start sending telemetry
+  var sendTemperatureInterval = setInterval(function () {
+    temperature += generateRandomIncrement();
+    var data = {
+      'temperature': temperature,
+      'temperature_unit': temperatureUnit
+    };
+    sendTelemetry(data, temperatureSchema)
+  }, 5000);
+
+  var sendHumidityInterval = setInterval(function () {
+    humidity += generateRandomIncrement();
+    var data = {
+      'humidity': humidity,
+      'humidity_unit': humidityUnit
+    };
+    sendTelemetry(data, humiditySchema)
+  }, 5000);
+
+  var sendPressureInterval = setInterval(function () {
+    pressure += generateRandomIncrement();
+    var data = {
+      'pressure': pressure,
+      'pressure_unit': pressureUnit
+    };
+    sendTelemetry(data, pressureSchema)
+  }, 5000);
+
+  client.on('error', function (err) {
+    printErrorFor('client')(err);
+    if (sendTemperatureInterval) clearInterval(sendTemperatureInterval);
+    if (sendHumidityInterval) clearInterval(sendHumidityInterval);
+    if (sendPressureInterval) clearInterval(sendPressureInterval);
+    client.close(printErrorFor('client.close'));
+  });
+}
+});
